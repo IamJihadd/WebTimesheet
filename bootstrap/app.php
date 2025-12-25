7bfd7409c394ddcf0cd0ddd4ddfcbc583432730d
xXxJHD <jihadfiisabilillah28@gmail.com>
2025-12-25 14:18:24 +0700

    update file bootstrap\app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,6 +16,8 @@ return Application::configure(basePath: dirname(__DIR__))
             'karyawan' => \App\Http\Middleware\EnsureUserIsKaryawan::class,
             'admin' => \App\Http\Middleware\EnsureUserIsAdmin::class,
         ]);
+
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
