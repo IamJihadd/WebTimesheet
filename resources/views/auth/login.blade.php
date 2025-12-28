218b1171916659d9ebe1d34f8323c3b9d9e824c4
xXxJHD <jihadfiisabilillah28@gmail.com>
2025-12-28 22:30:41 +0700

    update kedua text input agar lebih besar ketika di mobile

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -33,7 +33,7 @@
                     <div class="mt-2">
                         <input id="user_id" type="text" name="user_id" value="{{ old('user_id') }}" required
                             autofocus
-                            class="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6 max-sm:text-sm/8" />
+                            class="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6 max-sm:text-sm/10" />
                     </div>
                     @error('user_id')
                         <p class="mt-2 text-sm text-red-400">{{ $message }}</p>
@@ -55,7 +55,7 @@
                     </div>
                     <div class="mt-2">
                         <input id="password" type="password" name="password" required autocomplete="current-password"
-                            class="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500  sm:text-sm/6 max-sm:text-sm/8" />
+                            class="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500  sm:text-sm/6 max-sm:text-sm/10" />
                     </div>
                     @error('password')
                         <p class="mt-2 text-sm text-red-400">{{ $message }}</p>
