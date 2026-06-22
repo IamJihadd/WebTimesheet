8f060d71a7dc0a6459886cb46a9f8fb17a9c5f58
xXxJHD <jihadfiisabilillah28@gmail.com>
2026-06-22 23:13:03 +0700

    Alihkan storage laravel ke tmp

--- api/index.php
+++ api/index.php
@@ -1,7 +1,20 @@
 <?php
 
-echo "<h1>Halo! Mesin PHP di Vercel Berhasil Menyala!</h1>";
-echo "<p>Jika tulisan ini muncul, berarti Vercel normal dan masalahnya ada di dalam sistem Laravel.</p>";
+// 1. Tetap paksa tampilkan error jika ada
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
 
-// Kita matikan (comment) pemanggilan Laravel untuk sementara
-// require __DIR__ . '/../public/index.php';
+// 2. Alihkan semua file sementara (cache, views, logs) ke folder /tmp Vercel
+$_ENV['APP_CONFIG_CACHE'] = '/tmp/config.php';
+$_ENV['APP_EVENTS_CACHE'] = '/tmp/events.php';
+$_ENV['APP_PACKAGES_CACHE'] = '/tmp/packages.php';
+$_ENV['APP_ROUTES_CACHE'] = '/tmp/routes.php';
+$_ENV['APP_SERVICES_CACHE'] = '/tmp/services.php';
+$_ENV['VIEW_COMPILED_PATH'] = '/tmp';
+$_ENV['CACHE_STORE'] = 'array';
+$_ENV['SESSION_DRIVER'] = 'cookie';
+$_ENV['LOG_CHANNEL'] = 'stderr';
+
+// 3. Nyalakan aplikasi Laravel!
+require __DIR__ . '/../public/index.php';
