b3bb40979fe927ff8d0c2bf99e241f0b10894abd
xXxJHD <jihadfiisabilillah28@gmail.com>
2026-06-22 23:06:20 +0700

    Matikan platform check dan paksa tampilkan error

--- api/index.php
+++ api/index.php
@@ -1,3 +1,8 @@
 <?php
 
+// Paksa PHP menampilkan semua error ke layar browser
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
 require __DIR__ . '/../public/index.php';
